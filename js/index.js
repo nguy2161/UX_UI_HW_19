e0dff37f93f9cae14a4e15cdef97a3d951628764
nguy2161 <70927105+nguy2161@users.noreply.github.com>
2021-02-09 01:24:03 -0600

    Added Jquery

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loaded correctly!");
+
+document.getElementsByClassName('.button').addEventListener('click',fuction(){
+    document.querySelector('.modal').style.display='flex';
+});
+document.querySelector('.close').addEventListener('click',function(){
+
+})
